c649a96926b926e0087953efb51413fd7ff65253
amrah00 <amrah-50@hotmail.com>
2021-04-12 14:44:09 +1000

    Profile page
    -added profile page
    - fix contact page
    - remove extra code

--- matching-system/src/App.js
+++ matching-system/src/App.js
@@ -8,18 +8,20 @@ import Login from './Frontend/components/pages/Login page/Login.js';
 import About from './Frontend/components/pages/Home page/aboutPage';
 import contactUs from './Frontend/components/pages/Home page/Contact/contactUs';
 import Register from './Frontend/components/pages/Register page/Register';
+import Profile from './Frontend/components/pages/Profile page/Profile';
 
 function App() {
   return (
       <Router>
         <div className="App">
-          {/* <SearchBar/> */}
           <Switch>
             <Route exact path="/" component={About} />
             <Route exact path="/about" component={About} />
             <Route exact path="/login" component={Login} /> 
             <Route exact path="/contact" component={contactUs} /> 
             <Route exact path="/register" component={Register} /> 
+            <Route exact path="/profile" component={Profile} /> 
+
             
 
         
