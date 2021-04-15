3724ee46c9b4f74d02abce2f2070d4652edc2c85
William Crawford <s3599773@student.rmit.edu.au>
2021-04-15 18:11:17 +1000

    Basic addUser functionality added to frontend Register.js page.

--- frontend/src/components/pages/Register page/Register.js
+++ frontend/src/components/pages/Register page/Register.js
@@ -5,6 +5,7 @@ import { FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import HomeNaveBar from './../../Layout/Home layout/HomeNaveBar';
 import SearchBar from './../../Layout/Search bar/SearchBar';
 import './Register.css';
+import axios from "axios";
 
 
 class Register extends Component{
@@ -12,20 +13,27 @@ class Register extends Component{
 
     super(props);
     this.state = {
-      id:"",
+      firstName:"",
+      lastName: "",
+      email: "",
       username: "",
-      password: "",
-      account: ""
+      password: ""
     };
 
     this.onChange = this.onChange.bind(this);
-    // this.onSubmit = this.onSubmit.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
   
   onChange(e){
     this.setState({ [e.target.name]: e.target.value });
   }
- 
+
+  //Adds a user to the backend repository using data grabbed the component state
+  onSubmit(e) {
+    e.preventDefault();
+    let newUser = this.state;
+    axios.post('/user/save', newUser)
+  }
   
   render() {
       return (
@@ -34,44 +42,54 @@ class Register extends Component{
         <SearchBar/>
 
           <div>
-            <Form className="register-form">
+            <Form className="register-form" onSubmit={this.onSubmit}>
               <h1>Register</h1>
 
               <Form.Group controlId="formBasicEmail" className="txtbr">
                 <Form.Control
-                  type="email"
+                  type="text"
+                  name = "email"
                   placeholder="Enter email"
-                 
+                  value={this.state.email}
+                  onChange={this.onChange}
                 />
                 <Form.Text className="text-muted"></Form.Text>
               </Form.Group>
 
               <Form.Group controlId="formBasicPassword" className="txtbr">
                 <Form.Control
-                  type="password"
+                  type="text"
+                  name = "password"
                   placeholder="Password"
-                 
+                  value={this.state.password}
+                  onChange={this.onChange}
                 />
               </Form.Group>
-              <Form.Group controlId="formBasicText" className="txtbr">
+              <Form.Group controlId="formBasicFirstName" className="txtbr">
                 <Form.Control
                   type="text"
+                  name = "firstName"
                   placeholder="First Name"
-                 
+                  value={this.state.firstName}
+                  onChange={this.onChange}
                 />
               </Form.Group>
-              <Form.Group controlId="formBasicText" className="txtbr">
+              <Form.Group controlId="formBasicLastName" className="txtbr">
                 <Form.Control
                   type="text"
+                  name = "lastName"
                   placeholder="Last Name"
-                 
+                  value={this.state.lastName}
+                  onChange={this.onChange}
                 />
               </Form.Group>
 
               <Row>
                 <Button
+                    type="submit"
+                    value="Submit"
                   variant="primary"
-                  onClick={(event) => this.handleClick(event)}
+                  // onClick={(event) => this.handleClick(event)}
                   className="registerbtn"
                 >
                   Register
