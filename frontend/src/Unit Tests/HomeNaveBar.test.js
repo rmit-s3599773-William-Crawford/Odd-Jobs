90a0010f46c93f75605e194f0e6e0a4dbf295f36
amrah00 <amrah-50@hotmail.com>
2021-04-19 12:36:45 +1000

    Fix unit test library
    - Unit tests for HomNevebar( all tests passed)

--- frontend/src/Unit Tests/HomeNaveBar.test.js
+++ frontend/src/Unit Tests/HomeNaveBar.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Navbar from '../components/Layout/Home layout/HomeNaveBar.js';
+import HomeNaveBar from '../components/Layout/Home layout/HomeNaveBar.js';
 import { shallow, mount } from 'enzyme';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
 
-describe("<HomeNaveBar /> Navigation bar Unit Test", () => {
+Enzyme.configure({ adapter: new Adapter() });
+//pass
+describe("HomeNaveBar Unit Test", () => {
     it('should render to About page', () => {
         const component = mount(<HomeNaveBar />);
         const button = component.find('li').first().children(0)
@@ -18,7 +19,7 @@ describe("<HomeNaveBar /> Navigation bar Unit Test", () => {
     
     });
 
-  
+    //pass
     it('should render to Contact page', () => {
         const component = mount(<HomeNaveBar />);
         const button = component.find('li').first().children(0)
@@ -28,12 +29,23 @@ describe("<HomeNaveBar /> Navigation bar Unit Test", () => {
         expect(component.find('ul > li > a').at(2).prop('href')).toBe('/contact');
     });
 
+    //pass
+    it('should render to Profile  page', () => {
+        const component = mount(<HomeNaveBar />);
+        const button = component.find('li').first().children(0)
+        console.log(button)
+        button.simulate('click');
+
+        expect(component.find('ul > li > a').at(3).prop('href')).toBe('/profile');
+    });
+
+    //pass
     it('should render to Login page', () => {
         const component = mount(<HomeNaveBar />);
         const button = component.find('li').first().children(0)
         console.log(button)
         button.simulate('click');
 
-        expect(component.find('ul > li > a').at(3).prop('href')).toBe('/login');
+        expect(component.find('ul > li > a').at(4).prop('href')).toBe('/login');
     });
 });
