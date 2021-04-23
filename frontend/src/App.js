8a7e256c9402a43a1e83a125f8ddd05cfcef142d
amrah00 <amrah-50@hotmail.com>
2021-04-23 14:27:41 +1000

    Fix and update
    - Fixed footer to show in all pages
    - Change the navigation/login bar layout
    - Delete unused classes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-
 import About from "./components/pages/Home page/aboutPage"
 import Login from "./components/pages/Login page/Login";
 import contactUs from "./components/pages/Home page/Contact/contactUs";
 import Register from "./components/pages/Register page/Register";
 import Profile from "./components/pages/Profile page/Profile";
+import './index.css';
+  
 
 
 export class App extends Component {
@@ -20,18 +20,24 @@ export class App extends Component {
 
     render() {
         return (
-            <Router>
-                <div className="App">
-                    <Switch>
-                        <Route exact path="/" component={About} />
-                        <Route exact path="/about" component={About} />
-                        <Route exact path="/login" component={Login} />
-                        <Route exact path="/contact" component={contactUs} />
-                        <Route exact path="/register" component={Register} />
-                        <Route exact path="/profile" component={Profile} />
-                    </Switch>
-                </div>
-            </Router>
+            <>
+                <Router>
+                    <div className="App">
+                        <Switch>
+                            <Route exact path="/" component={About} />
+                            <Route exact path="/about" component={About} />
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/contact" component={contactUs} />
+                            <Route exact path="/register" component={Register} />
+                            <Route exact path="/profile" component={Profile} />
+                        </Switch>
+                    </div>
+                </Router>
+                    <div class="footer">
+                        MatchMe © 
+                        OddJobs Group 6
+                    </div>
+          </>
         );
     }
 }
