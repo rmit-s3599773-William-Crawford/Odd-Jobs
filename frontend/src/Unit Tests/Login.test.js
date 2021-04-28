3641d88fa3ea9b3f6f58b0ad8c0bbfe46a403a72
albert.nguyenn <s3718049@student.rmit.edu.au>
2021-04-28 19:09:45 +1000

    Login and Register Unit Tests

---
+++ frontend/src/Unit Tests/Login.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Login from '../components/pages/Login page/Login.js';
+import { shallow, mount } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("Login Unit Test", () => {
+
+
+    const wrapper = shallow(<Login />);
+    it('should have input for email and password', ()=> {
+        expect(wrapper.find('.txtb.form-group input[type="email"]')).toHaveLength(0);
+        expect(wrapper.find('.txtb.form-group input[type="password"]')).toHaveLength(0);
+       
+    })
+    
+    
+});
