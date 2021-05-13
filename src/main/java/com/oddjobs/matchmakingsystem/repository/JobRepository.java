ac2446a1285601475c3a8200f8efa1447b324d5b
s3720135 <s3720135@student.rmit.edu.au>
2021-05-13 12:16:59 +1000

    Create JobRepository.java
    added a job repository

---
+++ src/main/java/com/oddjobs/matchmakingsystem/repository/JobRepository.java
@@ -0,0 +1,8 @@
+package com.oddjobs.matchmakingsystem.repository;
+
+import com.oddjobs.matchmakingsystem.model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends jobRepository<Job, Long>{
+    
+}
